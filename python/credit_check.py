6b98e3ed7a818290641a009a3aa9cfefb8eced1e
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-26 16:21:49 -0500

    initial py solution

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,23 @@
-def credit_check(str):
-    pass
+from functools import total_ordering
+import functools
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+
+def credit_check(account_num):
+    dig_list= [int(a) for a in str(account_num)]
+    for i, x in enumerate(dig_list):
+        if(i % 2 == 0):
+            mult_dig= x*2
+            if(mult_dig>=10):
+                total=0
+                for b in str(mult_dig):
+                    total+=int(b)
+                dig_list[i] = total
+            else:
+                dig_list[i]=mult_dig                    
+    checker = functools.reduce(lambda agg, item:agg+item,dig_list)
+    if checker % 10 == 0:
+        return "The number is valid!"
+    else:
+        return "The number is invalid!"
+
 
